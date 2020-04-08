612c3c737a17ec4901e949f59ed3337b8c86d1c3
Shakthi Prasad Gs <shakthi.gs87@gmail.com>
2020-04-08 09:57:12 +0530

    Reusing multiple  statements

--- flourish/treeSitter/backend/fnode.js
+++ flourish/treeSitter/backend/fnode.js
@@ -4,23 +4,56 @@
 class FNode {
 
     constructor(node){
+        this.apply(node);
+        this.children = [];
+    }
+
+
+    apply(node){
         let data = {startPosition: node.startPosition, type: node.type, endPosition: node.endPosition };
         Object.assign(this,data);
-        this.children = [];
     }
 
 };
 
 
+function isEqaulNode(first,next){
+    if (first["0"] === next["0"])
+        return true;
 
+    if(first.childCount!=next.childCount)
+        return false;
 
+    let mismatch =first.children.find((child,index)=>{
+        return child["0"]!=next.children[index]["0"];
+    })
 
-function reConciliationNode(originalFnodeTree,originalTsTree,node)
-{
-    let fnode = new FNode(node)
-    fnode.children = node.children.map((child)=>reConciliationNode(originalFnodeTree,originalTsTree,child))
+    return !mismatch;
 
-    return fnode;
+}
+
+function reConciliationNode(originalFnodeTree, originalTsTree, node) {
+    if (originalTsTree == null) {
+        let fnode = new FNode(node)
+        fnode.children = node.children.map((child) => reConciliationNode(originalFnodeTree, originalTsTree, child))
+
+        return fnode;
+    } else {
+
+        if (isEqaulNode(originalTsTree,node)) {
+            originalFnodeTree.apply(node);
+
+            console.log("resusing",originalFnodeTree.type,node.text );
+            return originalFnodeTree;
+        } else {
+            let fnode = new FNode(node)
+            fnode.children = node.children.map((child, index) => reConciliationNode(originalFnodeTree.children[index], originalTsTree.children[index], child))
+
+            return fnode;
+        }
+
+    }
+
 }
 
 FNode.reConciliation = function (originalFnodeTree,originalTsTree,newTsTree) {
