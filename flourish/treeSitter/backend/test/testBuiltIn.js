324e35311736d7cf35e7843504b3d5985ba64fd6
Shakthi Prasad Gs <shakthi.gs87@gmail.com>
2020-05-02 13:30:15 +0530

    Better test message

--- flourish/treeSitter/backend/test/testBuiltIn.js
+++ flourish/treeSitter/backend/test/testBuiltIn.js
@@ -6,10 +6,16 @@ var Parser = require('../Parser.js');
 
 describe("builtIn", () => {
 
-    it('should eval environment', () => {   
+    it('should have add', () => {   
         let flourishParser = new Parser();  
         let tree = flourishParser.parse("add  1 2\n");      
         assert.equal(evaluate(tree.children[0].children[0],environment.create()),3)
+    });
+
+    it('should have subtract add', () => {   
+        let flourishParser = new Parser();  
+        let tree = flourishParser.parse("subtract  1 2\n");      
+        assert.equal(evaluate(tree.children[0].children[0],environment.create()),-1)
     });
 
 
