e906c42fc4129df16a73fa2f78dfbdc7f3d42b57
Shakthi Prasad Gs <shakthi.gs87@gmail.com>
2020-05-10 11:50:30 +0530

    Starting completion

---
+++ flourish/anykeyComplete.js
@@ -0,0 +1,26 @@
+// TreeSitter-CodeMirror addon, copyright (c) by Shakthi Prasad GS and others
+// Distributed under an MIT license: https://codemirror.net/LICENSE
+
+(function (mod) {
+    if (typeof exports == "object" && typeof module == "object") // CommonJS
+        mod(require("../../lib/codemirror"));
+    else if (typeof define == "function" && define.amd) // AMD
+        define(["../../lib/codemirror"], mod);
+    else // Plain browser env
+        mod(CodeMirror);
+})(function (CodeMirror) {
+
+    CodeMirror.defineOption("anyKeyCompletion", false, function (cm, val, old) {
+
+
+        if (instance.state.completionActive) {
+            return;
+        }
+        CodeMirror.commands.autocomplete(instance);
+
+    })
+
+
+
+
+})
