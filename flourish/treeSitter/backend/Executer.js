21afa72ee1645e90aded8de98b9636dcf688dd28
Shakthi <shakthi.gs87@gmail.com>
2021-01-10 22:43:44 +0530

    Executor init

---
+++ flourish/treeSitter/backend/Executer.js
@@ -0,0 +1,20 @@
+const evaluate = require('./evaluate');
+const envCreate = require('./environment').create;
+
+class Executer{
+
+
+    constructor(intree){
+        this.tree = intree;
+
+    }
+
+
+    execute(){
+    }
+
+
+
+}
+
+module.exports = Executer;
--- flourish/treeSitter/backend/index.js
+++ flourish/treeSitter/backend/index.js
@@ -1,50 +1,23 @@
 
 const server = require('http').createServer();
 const io = require('socket.io')(server);
+const Executer = require('./Executer');
 const Parser = require('./Parser')
-// const evaluate = require('./evaluate')
-const envCreate = require('./environment').create;
-
-const evaluate = function name(ast,env) {
-    if(!ast.isMutated)
-        console.log("Mutated",ast.leafText)
-    if(ast.children && ast.children.length){
-        ast.children.forEach(element => {
-            evaluate(element,env);
-        });
-    }
-}
 
 
+// const evaluate = function name(ast,env) {
+//     if(!ast.isMutated)
+//         console.log("Mutated",ast.leafText)
+//     if(ast.children && ast.children.length){
+//         ast.children.forEach(element => {
+//             evaluate(element,env);
+//         });
+//     }
+// }
+
+/**Patch tree for consumption of codemirror  */
 function patchTree(tree, env) {
     let errors = [];
-    tree.children = tree.children.map(mayBeStatement => {
-        if (mayBeStatement.type == 'statement') {
-            try {
-                let error = evaluate(mayBeStatement.children[0], env);
-                if (error && error.constructor === evaluate.ERROR) {
-                    if (!error.startPosition) {
-                        error.startPosition = mayBeStatement.startPosition;
-                        error.endPosition = mayBeStatement.endPosition;
-                    }
-                    
-                    if (error.startPosition.line == error.endPosition.line 
-                        && error.startPosition.column == error.endPosition.column) {
-                        error.startPosition.column -=1;
-                    }
-
-                    errors.push(error);
-                }
-            } catch (error) {
-                console.log("Unhandled error in eval", error);
-                errors.push(evaluate.ERROR.fromAst(mayBeStatement, `Unhandled error in eval ${error}`));
-            }
-
-        } else if (mayBeStatement.type == 'ERROR') {
-            errors.push(evaluate.ERROR.fromAst(mayBeStatement, 'Statement expected'));
-        }
-        return mayBeStatement;
-    });
     tree.errors = errors;
     return tree;
 }
@@ -54,20 +27,24 @@ function patchTree(tree, env) {
 io.on('connection', socket => {
 
 
-    let parser = new Parser();
+    let parser = null;
+    let executer = null;
     socket.on('parse', sourceCode => {
-        const environment = envCreate()
+        parser = new Parser();
         let outTree = parser.parse(sourceCode);
-        outTree = patchTree(outTree, environment);
+        executer = new Executer(outTree);
+        let result = executer.execute()
+        outTree = patchTree(outTree, result);
+
 
         socket.emit('parseComplete', outTree);
     });
 
     socket.on('parseIncremental', data => {
-        const environment = envCreate()
         const newSourceCode = data.newtext;
         let [tree, changes] = parser.parseIncremental(newSourceCode, data.posInfo)
-        tree = patchTree(tree, environment);
+        let result = executer.execute()
+        tree = patchTree(tree, result);
         tree.changes = changes;
         socket.emit('parseComplete', tree);
 
