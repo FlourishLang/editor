5122500858b307e9252838e5eb43315849185898
Shakthi Prasad Gs <shakthi.gs87@gmail.com>
2021-01-12 14:24:48 +0530

    Added yield to if executor.

--- flourish/treeSitter/backend/Executer.js
+++ flourish/treeSitter/backend/Executer.js
@@ -1,17 +1,17 @@
 const evaluate = require('./evaluate');
 const envCreate = require('./environment').create;
 
-class Executer{
+class Executer {
 
 
-    constructor(intree){
+    constructor(intree) {
         this.tree = intree;
         this.executor = executorFunction(this.tree);
 
     }
 
 
-    execute(state){
+    execute(state) {
         return this.executor.next(state);
     }
 
@@ -21,13 +21,75 @@ class Executer{
 
 module.exports = Executer;
 
+let ifExecutorFunction = function* ifExecutorFunction(tree) {
 
-let executorFunction = function * executorFunction(tree) {
+    yield "Nothing";
+
+}
+
+let executorFunction = function* executorFunction(tree) {
 
     //Run build loop
     while (true) {
-        
+        const environment = envCreate();
+        for (let index = 0; index < tree.children.length; index++) {
+            const mayBeStatement = tree.children[index];
+            if (mayBeStatement.type == 'statement') {
+                let result = null;
+                try {
+                    result = evaluate(mayBeStatement.children[0], environment);
+                } catch (error) {
+
+                    if(error === "Cannot evaluate:ifStatement"){
+                        yield* ifExecutorFunction(mayBeStatement);
+                    }
+                    
+                }
+
+                if (result && result.constructor === evaluate.ERROR) {
+                    throw (result);
+                }
+            } else if(mayBeStatement.type!="emptylines"){
+                throw (mayBeStatement);
+            }
+            
+        }
+
+
         yield "Nothing"
     }
-    
+
 }
+
+
+/*
+
+ tree.children = map(mayBeStatement => {
+            if (mayBeStatement.type == 'statement') {
+                try {
+                    let error = evaluate(mayBeStatement.children[0], env);
+                    if (error && error.constructor === evaluate.ERROR) {
+                        if (!error.startPosition) {
+                            error.startPosition = mayBeStatement.startPosition;
+                            error.endPosition = mayBeStatement.endPosition;
+                        }
+
+                        if (error.startPosition.line == error.endPosition.line
+                            && error.startPosition.column == error.endPosition.column) {
+                            error.startPosition.column -=1;
+                        }
+
+                        errors.push(error);
+                    }
+                } catch (error) {
+                    console.log("Unhandled error in eval", error);
+                    errors.push(evaluate.ERROR.fromAst(mayBeStatement, `Unhandled error in eval ${error}`));
+                }
+
+            } else if (mayBeStatement.type == 'ERROR') {
+                errors.push(evaluate.ERROR.fromAst(mayBeStatement, 'Statement expected'));
+            }
+            return mayBeStatement;
+        });
+
+*/
